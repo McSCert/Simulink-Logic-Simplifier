3b0c2e54f13f059c7ed591c066b82aad0e5b3442
Gordon Marks <gor.marks@gmail.com>
2017-07-15 20:39:59 +0000

    Pulled out the findMatchingParen function.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/SimulinkLogicSimplifier@10718 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/evaluateConstOps.m
+++ src/evaluateConstOps.m
@@ -213,45 +213,6 @@ catch
 end
 end
 
-function match = findMatchingParen(str, index)
-% Finds the '(' to open  a ')' indicated by a given index, or
-% finds the ')' to close a '(' indicated by a given index
-% Assumes str is a character array with balanced parentheses
-% Assumes index is the index of a '(' or a ')'
-
-%Code adapted from: https://stackoverflow.com/questions/12752225/how-do-i-find-the-position-of-matching-parentheses-or-braces-in-a-given-piece-of
-
-if strcmp(str(index), '(')
-    counter = 1;
-    while (counter > 0)
-        index = index + 1;
-        assert(index <= length(str), 'Closing bracket not found')
-        char = str(index);
-        if strcmp(char, '(')
-            counter = counter + 1;
-        elseif strcmp(char, ')')
-            counter = counter - 1;
-        end
-    end
-elseif strcmp(str(index), ')')
-    counter = 1;
-    while (counter > 0)
-        index = index - 1;
-        assert(index >= 1, 'Opening bracket not found')
-        char = str(index);
-        if strcmp(char, '(')
-            counter = counter - 1;
-        elseif strcmp(char, ')')
-            counter = counter + 1;
-        end
-    end
-else
-    error('Either ''('' or '')'' expected at indicated position, but not found.')
-end
-
-match = index;
-end
-
 % function [isAtomic, newStr] = handleOps1(str,matchIndex)
 % %version 1 - doesn't work for exprOPexprOPexpr if 1st 2 parts don't eval
 %         % Identify expr1 and expr2
