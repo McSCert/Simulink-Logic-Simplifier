8d7d0d28a5a65b0d5bf29ca7709e4617e0f4a952
Gordon Marks <gor.marks@gmail.com>
2017-08-03 20:43:01 +0000

    Added 3rd type for unary operators, added the unary minus, made the "~" unary. Made it error if an unrecognized operator is given.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/SimulinkLogicSimplifier@10838 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/getConnectiveBlock.m
+++ src/getConnectiveBlock.m
@@ -1,6 +1,10 @@
 function [opType, type] = getConnectiveBlock(connective)
 %Gets information for the connective block representing the connective
 
+% type = 2 -> unary
+% type = 1 -> relational
+% type = 0 -> logical
+
     switch connective
         case '&'
             type = 0;
@@ -12,8 +16,13 @@ function [opType, type] = getConnectiveBlock(connective)
             opType = connective;
             type = 1;
         case '~'
-            type = 0;
+            type = 2;
             opType = 'NOT';
+        case '-'
+            type = 2;
+            opType = 'UnaryMinus';
+        otherwise
+            error('Unexpected operator')
     end
 end
 
