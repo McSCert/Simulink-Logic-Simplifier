79df8d546fb79185b9868271ccac20b04aaac947
Gordon Marks <gor.marks@gmail.com>
2017-08-03 21:04:52 +0000

    Added some comments.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/SimulinkLogicSimplifier@10853 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/logicreducer/getSrcPorts.m
+++ src/logicreducer/getSrcPorts.m
@@ -1,8 +1,13 @@
-function srcPorts = getSrcPorts(blockName)
+function srcPorts = getSrcPorts(block)
+%
+% block can be either the name or the handle
+%
+% srcPorts is the handles of the ports which act as sources to the given
+%   block
 
     srcPorts = [];
     
-    lines = get_param(blockName, 'LineHandles');
+    lines = get_param(block, 'LineHandles');
     lines = lines.Inport;
     
     for i = 1:length(lines)
