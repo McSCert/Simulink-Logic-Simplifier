e41ed75d9a757c2bedd5f1c9269969e22cee8c71
Gordon Marks <gor.marks@gmail.com>
2017-08-03 20:49:38 +0000

    Changed implementation to use MakeNameUnique option instead of using our own code for that.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/SimulinkLogicSimplifier@10843 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/addLogicalBlock.m
+++ src/addLogicalBlock.m
@@ -1,18 +1,9 @@
 function block = addLogicalBlock(opType, sys)
 %adds a logical operator block of the specified optype
 
-    flag = 1;
-    num = 0;
     %makes sure added operator block has a unique name associated with it
-    while flag
-        try
-            block = add_block('built-in/Logic', [sys '/Generated' opType num2str(num)]);
-            flag = 0;
-        catch
-            flag = 1;
-            num = num + 1;
-        end
-    end
+    block = add_block('built-in/Logic', [sys '/Generated' opType], 'MAKENAMEUNIQUE','ON');
+    
     %actually adds the block
     set_param(block, 'Operator', opType);
 
