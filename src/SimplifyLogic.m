ec190c9da2cc205b018e76f3003a81c14523a9ff
Gordon Marks <gor.marks@gmail.com>
2017-09-20 23:04:21 +0000

    Added context menu option to verify after simplification. Added argument to SimplifyLogic to verify after simplification. Generated models are now saved in a folder at [pwd '/Logic_Simplifier_Results']. TODO - add call to verification function.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/SimulinkLogicSimplifier@11208 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/SimplifyLogic.m
+++ src/SimplifyLogic.m
@@ -1,13 +1,17 @@
-function [newExpr, oldExpr] = SimplifyLogic(blocks)
+function [newExpr, oldExpr] = SimplifyLogic(blocks, varargin)
 % SIMPLIFYLOGIC A function that takes a set of logic blocks and simplifies
-%   them.
+%   them. Results are saved in a new model in a folder called 
+%   'Logic_Simplifier_Results'.
 %
 %   Input:
-%       blocks  Cell array of blocks (indicated by fullname). All blocks
-%               should be in the same system/subsystem. Input blocks should
-%               be the outputs of the simplification (i.e. if the only only
-%               block is an outport, it will simplify the blocks that
-%               impact it).
+%       blocks      Cell array of blocks (indicated by fullname). All blocks
+%                   should be in the same system/subsystem. Input blocks should
+%                   be the outputs of the simplification (i.e. if the only only
+%                   block is an outport, it will simplify the blocks that
+%                   impact it).
+%       varargin{1} Bool indicating whether or not to verify the results.
+%                   Verifying involves creating an additional model within the
+%                   'Logic_Simplifier_Results' folder.
 %
 %   Outputs:
 %       oldExpr     Cell array of expressions found for the blocks as
@@ -19,7 +23,15 @@ function [newExpr, oldExpr] = SimplifyLogic(blocks)
 DELETE_UNUSED = getLogicSimplifierConfig('delete_unused', 'off'); % Indicates whether or not to delete blocks which are unused in the final model
 SUBSYSTEM_RULE = getLogicSimplifierConfig('subsystem_rule', 'blackbox'); % Indicates how to address subsystems in the simplification process
 % Non-config constant:
-REPLACE_EXISTING_MODEL = 'off'; % When creating the model for the simplification, it will replace a file with the same name if 'on' otherwise it will error
+REPLACE_EXISTING_MODEL = 'on'; % When creating the model for the simplification, it will replace a file with the same name if 'on' otherwise it will error
+
+if nargin == 1
+    verify = false;
+elseif nargin == 2
+    verify = varargin{1};
+else
+    error(['Error in ' mfilename ', 1 or 2 input arguments expected.']);
+end
 
 parent = get_param(blocks{1}, 'parent'); % Get name of system the blocks are in
 origModel = bdroot(blocks{1});
@@ -29,27 +41,46 @@ if ~(strcmp(get_param(origModel, 'UnderspecifiedInitializationDetection'), 'Clas
     disp(['Warning: The ' mfilename ' function may result in unexpected results if the ''UnderspecifiedInitializationDetection'' model parameter is not set to ''Classic'', please check the results carefully.'])
 end
 
+
 % Create a new system for the simplification
 parentName = get_param(parent, 'Name');
+logicSys = [parentName '_newLogic'];
 try
-    logicSys = new_system([parentName '_newLogic']); % This will error if it's already open
-catch ME
     if strcmp(REPLACE_EXISTING_MODEL, 'off')
-        rethrow(ME)
+        logicSys = new_system(logicSys);
     elseif strcmp(REPLACE_EXISTING_MODEL, 'on')
-        close_system([parentName '_newLogic'], 0); % Don't save
-        logicSys = new_system([parentName '_newLogic']);
+        saveFlag = 0; % i.e. don't save
+        close_system(logicSys, saveFlag);
+        logicSys = new_system(logicSys);
     else
         error(['Error in ' mfilename ', REPLACE_EXISTING_MODEL should be ''on'' or ''off''.']);
     end
+catch ME
+    if ~strcmp(ME.identifier, 'Simulink:LoadSave:InvalidBlockDiagramName')
+        % Name invalid so use some default
+        logicSys = 'Default_SimplifiedSys';
+        if strcmp(REPLACE_EXISTING_MODEL, 'off')
+            logicSys = new_system(logicSys);
+        elseif strcmp(REPLACE_EXISTING_MODEL, 'on')
+            saveFlag = 0; % i.e. don't save
+            close_system(logicSys, saveFlag);
+            logicSys = new_system(logicSys);
+        else
+            error(['Error in ' mfilename ', REPLACE_EXISTING_MODEL should be ''on'' or ''off''.']);
+        end
+    else
+        rethrow(ME)
+    end
 end
-open_system(logicSys);
+open_system(logicSys)
 set_param(logicSys, 'Solver', get_param(origModel, 'Solver'));
 set_param(logicSys, 'SolverType', get_param(origModel, 'SolverType'));
 set_param(logicSys, 'ProdHWDeviceType', get_param(origModel, 'ProdHWDeviceType'));
 set_param(logicSys, 'UnderspecifiedInitializationDetection', get_param(origModel, 'UnderspecifiedInitializationDetection'));
 
+% Perform the simplification and generate the simplification in logicSys
 [newExpr, oldExpr] = doSimplification(logicSys, blocks);
+
 
 if strcmp(DELETE_UNUSED,'on')
     % Delete blocks in the top-level system that don't contribute to output
@@ -83,4 +114,62 @@ if ~strcmp(SUBSYSTEM_RULE, 'blackbox')
         set_param(getfullname(subsystems(i)), 'Zoomfactor', '100');
     end
 end
+
+% Save the resulting model
+startDir = pwd;
+resultsDir = 'Logic_Simplifier_Results';
+mkdir(resultsDir) % Where we'll save results
+cd([startDir '/' resultsDir])
+save_system(logicSys)
+cd(startDir)
+
+if verify
+    if strcmp(parent, bdroot(parent)) % parent is the whole model
+        %% TODO - Call verification function on logicSys and parent
+    else
+        % Extract subsystem to new model
+
+        copySys = parentName;
+        try
+            if strcmp(REPLACE_EXISTING_MODEL, 'off')
+                copySys = new_system(copySys, 'Model', parent);
+            elseif strcmp(REPLACE_EXISTING_MODEL, 'on')
+                saveFlag = 0; % i.e. don't save
+                close_system(copySys, saveFlag);
+                copySys = new_system(copySys, 'Model', parent);
+            else
+                error(['Error in ' mfilename ', REPLACE_EXISTING_MODEL should be ''on'' or ''off''.']);
+            end
+        catch ME
+            if ~strcmp(ME.identifier, 'Simulink:LoadSave:InvalidBlockDiagramName')
+                % Name invalid so use some default
+                copySys = 'Deafult_CopiedSys';
+                if strcmp(REPLACE_EXISTING_MODEL, 'off')
+                    copySys = new_system(copySys, 'Model', parent);
+                elseif strcmp(REPLACE_EXISTING_MODEL, 'on')
+                    saveFlag = 0; % i.e. don't save
+                    close_system(copySys, saveFlag);
+                    copySys = new_system(copySys, 'Model', parent);
+                else
+                    error(['Error in ' mfilename ', REPLACE_EXISTING_MODEL should be ''on'' or ''off''.']);
+                end
+            else
+                rethrow(ME)
+            end
+        end
+        open_system(copySys)
+
+        set_param(copySys, 'Solver', get_param(origModel, 'Solver'));
+        set_param(copySys, 'SolverType', get_param(origModel, 'SolverType'));
+        set_param(copySys, 'ProdHWDeviceType', get_param(origModel, 'ProdHWDeviceType'));
+        set_param(copySys, 'UnderspecifiedInitializationDetection', get_param(origModel, 'UnderspecifiedInitializationDetection'));
+        
+        cd([startDir '/' resultsDir])
+        save_system(copySys)
+        cd(startDir)
+
+        %% TODO - Call verification function on logicSys and copySys
+    end
+end
+
 end
--- src/sl_customization.m
+++ src/sl_customization.m
@@ -26,12 +26,32 @@ end
 
 %% Define first item
 function schema = LogicSimplifier(callbackInfo)
+    schema = sl_container_schema;
+    schema.label = 'Logic Simplifier';
+    schema.childrenFcns = {@getSimplify, @getSimplifyWithVerify};
+end
+
+%% Define Simplify Logic Option
+function schema = getSimplify(callbackInfo)
     schema = sl_action_schema;
     schema.label = 'Simplify Logic';
     schema.userdata = 'simplifylogic';
     schema.callback = @SimplifyLogicCallback;
 end
 
+%% Define Simplify Logic With Verify Option
+function schema = getSimplifyWithVerify(callbackInfo)
+    schema = sl_action_schema;
+    schema.label = 'Simplify Logic and Verify Results';
+    schema.userdata = 'simplifylogicandverify';
+    schema.callback = @SimplifyLogicWithVerifyCallback;
+end
+
 function SimplifyLogicCallback(callbackInfo)
     SimplifyLogic(gcbs);
+end
+
+function SimplifyLogicWithVerifyCallback(callbackInfo)
+    verify = true;
+    SimplifyLogic(gcbs, verify);
 end
