e69ff84f9b8ca62197a29dd3cfabb6c728992e6a
Gordon Marks <gor.marks@gmail.com>
2017-09-21 20:20:56 +0000

    Small fix.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/SimulinkLogicSimplifier@11216 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/Utility/new_system_makenameunique.m
+++ src/Utility/new_system_makenameunique.m
@@ -1,12 +1,13 @@
 function h = new_system_makenameunique(baseName, varargin)
 % Use varargin to pass additional arguments to the new_system command
 
-if exist(baseName, 'file') == 4
+name = baseName;
+if exist(name, 'file') == 4
     n = 1;
-    while exist(strcat(baseName, num2str(n)), 'file') == 4
+    while exist(strcat(name, num2str(n)), 'file') == 4
         n = n + 1;
     end
-    name = strcat(baseName, num2str(n));
+    name = strcat(name, num2str(n));
 end
 
 h = new_system(name,varargin{:});
