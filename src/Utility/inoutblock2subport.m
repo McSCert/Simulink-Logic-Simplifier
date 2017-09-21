ac6137bf6a41ff6457c2897abebf081cc3ef74fd
Monika Jaskolka <bialy.monika@gmail.com>
2017-09-21 17:45:35 +0000

    Updating two functions so that they support Model References.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/SimulinkLogicSimplifier@11210 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/Utility/inoutblock2subport.m
+++ src/Utility/inoutblock2subport.m
@@ -1,28 +1,52 @@
 function subPort = inoutblock2subport(inoutBlock)
+% INOUTBLOCK2SUBPORT Get the SubSystem or ModelReference port handle corresponding 
+%   to an Inport/Outport block.
 %
-% Get the SubSystem port handle which corresponds with a given Inport or
-% Outport block.
+%   Inputs:
+%       inoutBlock  Inport or outport block.
 %
-% If the Inport/Outport is at the top-level of a model, subPort will be [].
+%   Outputs:
+%       subPort     Port handle, or [] if top-level of a model.
+%
+%   Example:
+%       hdl = inoutblock2subport(gcb)
 
-blockType = get_param(inoutBlock, 'BlockType');
-assert(strcmp(blockType,'Inport') || strcmp(blockType,'Outport'), 'Unexpected input block type.')
+    blockType = get_param(inoutBlock, 'BlockType');
+    assert(strcmp(blockType,'Inport') || strcmp(blockType,'Outport'), ...
+        'Unexpected input block type.')
 
-pNum = str2double(get_param(inoutBlock, 'Port'));
-parent = get_param(inoutBlock, 'Parent');
-if strcmp(get_param(parent, 'Type'), 'block_diagram')
-    subPort = [];
-else
-    subPorts = get_param(parent, 'PortHandles');
-    subPorts = getfield(subPorts, blockType);
-    
-    for i = 1:length(subPorts)
-        if get_param(subPorts(i), 'PortNumber') == pNum
-            subPort = subPorts(i);
-            return
+    pNum = str2double(get_param(inoutBlock, 'Port'));
+    parent = get_param(inoutBlock, 'Parent');
+    if strcmp(get_param(parent, 'Type'), 'block_diagram')
+        referenceParent = get_param(parent, 'ModelReferenceParent');
+            
+        if referenceParent % Root of model but has a Model Reference block as parent
+            [mdls, mdlblks] = find_mdlrefs(referenceParent, 'AllLevels', false);
+            idx = find(ismember(mdls, parent));
+            referenceBlock = cell2mat(mdlblks(idx));
+            subPorts = get_param(referenceBlock, 'PortHandles');
+            subPorts = getfield(subPorts, blockType);
+            
+            for i = 1:length(subPorts)
+                if get_param(subPorts(i), 'PortNumber') == pNum
+                    subPort = subPorts(i);
+                    return
+                end
+            end
+            
+        else % Root of a model
+            subPort = [];
         end
-    end
-    assert(exist('subPort', 'var'))
-end
+    else 
+        subPorts = get_param(parent, 'PortHandles');
+        subPorts = getfield(subPorts, blockType);
 
+        for i = 1:length(subPorts)
+            if get_param(subPorts(i), 'PortNumber') == pNum
+                subPort = subPorts(i);
+                return
+            end
+        end
+        assert(exist('subPort', 'var'))
+    end
 end
--- src/Utility/subport2inoutblock.m
+++ src/Utility/subport2inoutblock.m
@@ -1,24 +1,40 @@
 function inoutBlock = subport2inoutblock(subPort)
+% SUBPORT2INOUTBLOCK Get the Inport/Outport block name corresponding to a SubSystem 
+%   or ModelReference Inport/Outport.
 %
-% Get the Inport or Outport block which corresponds with a given SubSystem
-% Inport or Outport.
+%   Inputs:
+%       subPort         Port handle.
 %
-% inoutBlock is returned as a cell array because there may be more than one
-% corresponding block.
+%   Outputs:
+%       inoutBlock      Cell array of names (path).
+%
+%   Example:
+%       ports = get_param(gcb, 'PortHandles');
+%       firstInport = ports.Inport(1);
+%       blkName = subport2inoutblock(firstInport);
 
-parent = get_param(subPort, 'Parent');
+    parent = get_param(subPort, 'Parent');
 
-assert(strcmp(get_param(subPort, 'Type'), 'port'), ...
-    'Input is expected to be a port handle.')
-portType = get_param(subPort, 'PortType');
+    % Input checks
+    assert(strcmp(get_param(subPort, 'Type'), 'port'), ...
+        'Input is expected to be a port handle.')
+    portType = get_param(subPort, 'PortType');
+    
+    parentType = get_param(parent, 'BlockType');
+    assert(strcmp(parentType, 'SubSystem') || strcmp(parentType, 'ModelReference'), ...
+        'Input is expected to belong to a SubSystem or Model Reference.')
 
-assert(strcmp(get_param(parent, 'BlockType'), 'SubSystem'), ...
-    'Input is expected to belong to a SubSystem.')
+    assert(strcmp(portType, 'outport') || strcmp(portType, 'inport'), ...
+        'Input is expected to be either an inport or outport.')
 
-assert(strcmp(portType, 'outport') || strcmp(portType, 'inport'), ...
-    'Input is expected to be either an inport or outport.')
-
-pNum = get_param(subPort, 'PortNumber');
-blockType = [upper(portType(1)), portType(2:end)];
-inoutBlock = find_system(parent, 'SearchDepth', 1, 'BlockType', blockType, 'Port', num2str(pNum));
+    % Match up port numbers to get the name
+    pNum = get_param(subPort, 'PortNumber');
+    blockType = [upper(portType(1)), portType(2:end)]; % Capitalize
+    if strcmp(parentType, 'ModelReference')
+        inoutBlock = cell2mat(find_in_models(parent, 'SearchDepth', 1, 'BlockType', ...
+            blockType, 'Port', num2str(pNum)));
+    else
+        inoutBlock = cell2mat(find_system(parent, 'SearchDepth', 1, 'BlockType', ...
+            blockType, 'Port', num2str(pNum)));
+    end
 end
