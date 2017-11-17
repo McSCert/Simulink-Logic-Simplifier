a200ea914e078f9640ea1672386b9f16f0455595
Gordon Marks <gor.marks@gmail.com>
2017-11-17 22:54:10 +0000

    Bug fix. Previously tried to create blocks in the same subsystem they were found in instead of appropriately deciding based on subsystem rule.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/SimulinkLogicSimplifier@11593 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/CreatingExpressions/createExpr.m
+++ src/CreatingExpressions/createExpr.m
@@ -105,7 +105,7 @@ end
 
 function [e_bh, e_blk] = createBlockCopy(s_blk, startSys, createIn, s2e_blockHandles)
 
-e_blk = regexprep(s_blk,['^' startSys], createIn, 'ONCE'); % Default name of the block to put in endSys
+e_blk = [createIn '/' get_param(s_blk, 'Name')]; % Default name of the block to put in endSys
 
 % Create block
 e_bh = add_block(s_blk, e_blk, 'MakeNameUnique', 'On');
