aa87c22a87d87d29d9428b82e573e9c5439072b9
Monika Jaskolka <bialy.monika@gmail.com>
2017-12-13 16:58:26 +0000

    Indented everyting between the function/end. Minor formatting.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/SimulinkLogicSimplifier@11760 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/Utility/getPorts.m
+++ src/Utility/getPorts.m
@@ -1,8 +1,8 @@
 function ports = getPorts(blk, type)
-% GETPORTS Gets the ports meeting a type constraint (see below) for a given 
+% GETPORTS Get the ports meeting a type constraint (see below) for a given 
 %   block.
 %
-%   Input:
+%   Inputs:
 %       blk     Fullname or handle of a block.
 %       type    Char array indicating the type of port. 
 %               Any single port type is accepted.
@@ -10,27 +10,27 @@ function ports = getPorts(blk, type)
 %               'In' indicates all incoming ports (everything except Outports).
 %               'Special' indicates all ports other than Inports/Outports.
 %
-%   Output:
-%       ports   List of handles
+%   Outputs:
+%       ports   List of handles.
 
-ph = get_param(blk, 'PortHandles');
-pfields = fieldnames(ph);
-ports = [];
+    ph = get_param(blk, 'PortHandles');
+    pfields = fieldnames(ph);
+    ports = [];
 
-switch type
-    case 'All'
-        for i=1:length(pfields) % for all field types
-            ports = [ports, ph.(pfields{i})];
-        end
-    case 'In'
-        for i=setdiff(1:length(pfields), 2) % for all inport field types
-            ports = [ports, ph.(pfields{i})];
-        end
-    case 'Special'
-        for i=3:length(pfields) % for everything other than in/out ports
-            ports = [ports, ph.(pfields{i})];
-        end
-    otherwise
-        ports = ph.(type);
-end
+    switch type
+        case 'All'
+            for i = 1:length(pfields) % for all field types
+                ports = [ports, ph.(pfields{i})];
+            end
+        case 'In'
+            for i = setdiff(1:length(pfields), 2) % for all inport field types
+                ports = [ports, ph.(pfields{i})];
+            end
+        case 'Special'
+            for i = 3:length(pfields) % for everything other than in/out ports
+                ports = [ports, ph.(pfields{i})];
+            end
+        otherwise
+            ports = ph.(type);
+    end
 end
