2c88c289577130c6bfb12dfe6c9a2baa9a33b6ad
Gordon Marks <gor.marks@gmail.com>
2018-04-03 01:48:20 +0000

    Function that gets the identifiers in a given expression.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/SimulinkLogicSimplifier@12443 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ src/getIdentifiers.m
@@ -0,0 +1,30 @@
+function [idsChar, idsCell] = getIdentifiers(expr)
+    % GETIDENTIFIERS Gets the identifiers/variables in expr
+    %
+    %   Input:
+    %       expr    A cell array representing an expression
+    %
+    %   Output:
+    %       idsChar     Character array of the identifiers. Identifiers are
+    %                   separated by spaces.
+    %       idsCell     Cell array of the identifiers
+    %
+    
+    % Example:
+    % expr = 'h 2 _3 TRUE e2l>lo h TRUE2 as 1a1 1_1 FALSE aFALSE 1a|)qwe'
+    % getIdentifiers(expr) -> 'h e2l lo TRUE2 as a1 aFALSE a qwe '
+    
+    % Get all strings of word characters starting with a letter and
+    % excluding 'TRUE', 'FALSE', and numbers. In particular, brackets,
+    % spaces, operators, 'TRUE', 'FALSE', and numbers will be excluded.
+    pat = '([a-zA-Z][\w]*)'; % Pattern for strings of word characters not starting with a digit or underscore
+    tok = regexp(expr, pat, 'tokens');
+    idsChar = [];
+    idsCell = {};
+    for i = 1:length(tok)
+        if ~any(strcmp(tok{i}{1}, [{'TRUE', 'FALSE'}, idsCell])) % If not TRUE or FALSE and not already accounted for
+            idsChar = [idsChar, tok{i}{1}, ' '];
+            idsCell = [idsCell, tok{i}];
+        end
+    end
+end
