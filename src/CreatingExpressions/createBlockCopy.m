28f2fbd32f1de56ed2d1f2d4b8f979b9e6922959
Gordon Marks <gor.marks@gmail.com>
2018-05-01 16:10:44 +0000

    Updated to use move_block which doesn't have some common errors when copying blocks.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/SimulinkLogicSimplifier@12629 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/CreatingExpressions/createBlockCopy.m
+++ src/CreatingExpressions/createBlockCopy.m
@@ -1,13 +1,11 @@
 function [e_bh, e_blk] = createBlockCopy(s_blk, startSys, createIn, s2e_blockHandles)
-
-e_blk = [createIn '/' get_param(s_blk, 'Name')]; % Default name of the block to put in endSys
-
-% Create block
-e_bh = add_block(s_blk, e_blk, 'MakeNameUnique', 'On');
-e_blk = getfullname(e_bh);
-
-% Record that the created block is related to s_blk
-s_bh = get_param(s_blk, 'Handle');
-s2e_blockHandles(s_bh) = e_bh; % This is a map object so it will be updated
-
+    
+    % Create block
+    e_bh = move_block(s_blk, createIn);
+    e_blk = getfullname(e_bh);
+    
+    % Record that the created block is related to s_blk
+    s_bh = get_param(s_blk, 'Handle');
+    s2e_blockHandles(s_bh) = e_bh; % This is a map object so it will be updated
+    
 end
