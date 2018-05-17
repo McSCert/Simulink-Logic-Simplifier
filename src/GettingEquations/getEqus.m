7040945e1d55070f89178bbf0770db1bdd7f044e
Gordon Marks <gor.marks@gmail.com>
2018-05-17 04:28:14 +0000

    Mostly a formatting edit.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/SimulinkLogicSimplifier@12780 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/GettingEquations/getEqus.m
+++ src/GettingEquations/getEqus.m
@@ -1,108 +1,108 @@
 function [newEqus, handleID] = getEqus(startSys, h, blocks, lhsTable, subsystem_rule, extraSupport)
-% GETEXPRS Get a list of equations to represent the value of h. If an
-% equation has previously been done (indicated by lhsTable), then that
-% equation won't be generated again and won't be included in the output.
-
-
-% Notes/rough overview:
-%   If h is an input port, then create an equation which sets it to the
-%       output port which sends a signal to h
-%   If the block/mask type corresponding with h is not supported or not listed 
-%       in blocks, then create a blackbox equation for it
-%   If it is supported and listed in blocks, then create an appropriate
-%       equation for the block given using it's next sources as input
-%       "next source" will generally be an input port of a block
-%   When creating equations, for any input to that equation, find the
-%       equation for that input (through a recursive call)
-
-if ~lhsTable.lookup.isKey(h)
-    % Get equation type - 'blk', 'in', or 'out'
-    eType = equationType(h);
+    % GETEQUS Get a list of equations to represent the value of h. If an
+    % equation has previously been done (indicated by lhsTable), then that
+    % equation won't be generated again and won't be included in the output.
     
-    % Get block type
-    blk = getBlock(h);
-    bType = get_param(blk, 'BlockType');
     
-    isMask = strcmp(get_param(blk, 'Mask'), 'on');
-    % Get mask type if it exists and determine if the mask/block type is
-    % supported
-    if isMask
-        mType = get_param(blk, 'MaskType');
-        isSupp = isSupportedMaskType(mType);
-    else
-        isSupp = isSupportedBlockType(bType);
-    end
+    % Notes/rough overview:
+    %   If h is an input port, then create an equation which sets it to the
+    %       output port which sends a signal to h
+    %   If the block/mask type corresponding with h is not supported or not listed
+    %       in blocks, then create a blackbox equation for it
+    %   If it is supported and listed in blocks, then create an appropriate
+    %       equation for the block given using it's next sources as input
+    %       "next source" will generally be an input port of a block
+    %   When creating equations, for any input to that equation, find the
+    %       equation for that input (through a recursive call)
     
-    % Figure out what to call the handle in equations
-    % Note: The type variable below can't be used to reliably get type data,
-    % its purpose is just to be human readable for analysis.
-    if isMask
-        type = [mType '_' eType '_'];
-    else
-        type = [bType '_' eType '_'];
-    end
-    type = regexprep(regexprep(type,'([^A-Za-z0-9])',''), '(^[0-9]*)', ''); % Strip characters that aren't valid for the equation structure
-    handleID = getUniqueId(type, lhsTable);
-    % Add to lhsTable
-    lhsTable.add(h, handleID); % This will tell us not to get this equation again (apart from the main use of lhsTable)
-    
-    inBlx = any(strcmp(blk, blocks));
-    switch eType
-        case 'blk'
-            [inExtraSupp, newEqus] = extraSupport(startSys, h, handleID, blocks, lhsTable, subsystem_rule, extraSupport);
-            if ~inExtraSupp
-                if inBlx && isMask && isSupp
-                    newEqus = getSuppMaskBlkEquation();
-                elseif inBlx && isSupp
-                    newEqus = getSuppBlkEquation();
-                else
-                    % Block is not supported or it isn't in blocks (blocks
-                    % designates the set of blocks we want to simplify)
-                    
-                    % Treat as blackbox
-                    newEqus = getBlackBoxEquation();
-                end
-            end
-        case 'out'
-            [inExtraSupp, newEqus] = extraSupport(startSys, h, handleID, blocks, lhsTable, subsystem_rule, extraSupport);
-            if ~inExtraSupp
-                if ~inBlx || (isMask && ~isSupp) || (~isMask && ~isSupp)
-                    % Block is not supported or it isn't in blocks (blocks
-                    % designates the set of blocks we want to simplify)
-                    
-                    % Treat as blackbox
-                    newEqus = getBlackBoxEquation();
-                else
-                    if isMask
-                        newEqus = getSuppMaskOutEquation();
+    if ~lhsTable.lookup.isKey(h)
+        % Get equation type - 'blk', 'in', or 'out'
+        eType = equationType(h);
+        
+        % Get block type
+        blk = getBlock(h);
+        bType = get_param(blk, 'BlockType');
+        
+        isMask = strcmp(get_param(blk, 'Mask'), 'on');
+        % Get mask type if it exists and determine if the mask/block type is
+        % supported
+        if isMask
+            mType = get_param(blk, 'MaskType');
+            isSupp = isSupportedMaskType(mType);
+        else
+            isSupp = isSupportedBlockType(bType);
+        end
+        
+        % Figure out what to call the handle in equations
+        % Note: The type variable below can't be used to reliably get type data,
+        % its purpose is just to be human readable for analysis.
+        if isMask
+            type = [mType '_' eType '_'];
+        else
+            type = [bType '_' eType '_'];
+        end
+        type = regexprep(regexprep(type,'([^A-Za-z0-9])',''), '(^[0-9]*)', ''); % Strip characters that aren't valid for the equation structure
+        handleID = getUniqueId(type, lhsTable);
+        % Add to lhsTable
+        lhsTable.add(h, handleID); % This will tell us not to get this equation again (apart from the main use of lhsTable)
+        
+        inBlx = any(strcmp(blk, blocks));
+        switch eType
+            case 'blk'
+                [inExtraSupp, newEqus] = extraSupport(startSys, h, handleID, blocks, lhsTable, subsystem_rule, extraSupport);
+                if ~inExtraSupp
+                    if inBlx && isMask && isSupp
+                        newEqus = getSuppMaskBlkEquation();
+                    elseif inBlx && isSupp
+                        newEqus = getSuppBlkEquation();
                     else
-                        newEqus = getSuppOutEquation();
+                        % Block is not supported or it isn't in blocks (blocks
+                        % designates the set of blocks we want to simplify)
+                        
+                        % Treat as blackbox
+                        newEqus = getBlackBoxEquation();
                     end
                 end
-            end
-        case 'in'
-            % Get the source
-            srch = getSrcPorts(h); % DstPort of a block which connects to this
-            assert(length(srch) == 1, 'Error, an input port is expected to have a single source.')
-            
-            % Get the equation for the handle and its sources recursively
-            [srcEqus, srcID] = getEqus(startSys, srch, blocks, lhsTable, subsystem_rule, extraSupport);
-            
-            equ = [handleID ' = ' srcID]; % This block/port's equation with respect to its sources
-            newEqus = [{equ}, srcEqus]; % Equations involved in this block/port's equation
-        otherwise
-            error('Error: Unexpected eType')
+            case 'out'
+                [inExtraSupp, newEqus] = extraSupport(startSys, h, handleID, blocks, lhsTable, subsystem_rule, extraSupport);
+                if ~inExtraSupp
+                    if ~inBlx || (isMask && ~isSupp) || (~isMask && ~isSupp)
+                        % Block is not supported or it isn't in blocks (blocks
+                        % designates the set of blocks we want to simplify)
+                        
+                        % Treat as blackbox
+                        newEqus = getBlackBoxEquation();
+                    else
+                        if isMask
+                            newEqus = getSuppMaskOutEquation();
+                        else
+                            newEqus = getSuppOutEquation();
+                        end
+                    end
+                end
+            case 'in'
+                % Get the source
+                srch = getSrcPorts(h); % DstPort of a block which connects to this
+                assert(length(srch) == 1, 'Error, an input port is expected to have a single source.')
+                
+                % Get the equation for the handle and its sources recursively
+                [srcEqus, srcID] = getEqus(startSys, srch, blocks, lhsTable, subsystem_rule, extraSupport);
+                
+                equ = [handleID ' = ' srcID]; % This block/port's equation with respect to its sources
+                newEqus = [{equ}, srcEqus]; % Equations involved in this block/port's equation
+            otherwise
+                error('Error: Unexpected eType')
+        end
+    else
+        handleID = lhsTable.lookup(h);
+        newEqus = {};
     end
-else
-    handleID = lhsTable.lookup(h);
-    newEqus = {};
-end
-
+    
     function neq = getBlackBoxEquation()
         
         % Get the source ports of the blk (i.e. inport, enable, ifaction, etc.)
         srcHandles = getPorts(blk, 'In');
-                
+        
         % Get the equations for the sources
         neq = {};
         equ = [handleID ' =? ']; % Note the notation '=?' being used for blackboxes
@@ -117,7 +117,7 @@ end
         
         neq = [neq, {equ}];
     end
-
+    
     function neq = getSuppBlkEquation()
         % Get equation for unmasked handles with eType of 'blk'
         switch bType
@@ -167,7 +167,7 @@ end
                 error('Error, unsupported BlockType when supported type expected.')
         end
     end
-
+    
     function neq = getSuppMaskBlkEquation()
         % Get equation for masked handles with eType of 'blk'
         switch mtype
@@ -176,7 +176,7 @@ end
                 error('Error, unsupported MaskType when supported type expected.')
         end
     end
-
+    
     function neq = getSuppOutEquation()
         % Get equation for unmasked handles with eType of 'out'
         
@@ -278,7 +278,7 @@ end
                 % Get the source ports of the blk (i.e. inports)
                 ph = get_param(blk, 'PortHandles');
                 srcHandles = ph.Inport;
-            
+                
                 assert(length(srcHandles) == 3) % IfElseif requires condition, then case, and else case
                 
                 % Get source equations
@@ -320,13 +320,11 @@ end
                 end
             case 'DataStoreRead'
                 neq = getBlackBoxEquation();
-            case 'Merge'
-            
             otherwise
                 error('Error, unsupported BlockType when supported type expected.')
         end
     end
-
+    
     function neq = getSuppMaskOutEquation()
         % Get equation for masked handles with eType of 'out'
         switch mType
@@ -339,36 +337,36 @@ end
 end
 
 function uid = getUniqueId(type, existingIDs)
-% GETUNIQUEID Gets a unique identifier for a block to use in equations
-%   (rather than having to write the fullpath).
-%
-%   Inputs:
-%       type        The type of identifier we want. (E.g. blocktype)
-%       existingIDs A bimap object (comes with Logic Simplifier).
-%                   If existingIDs.lookdown('id') is defined then 'id'
-%                   is an existing identifier.
-%
-%   Output:
-%       uid     Character array for a unique identifier. Uses a type to
-%               assign the identifier and appends a number to the end to
-%               keep it different from existing identifiers.
-
-assert(ischar(type), '''type'' variable expected to be char.')
-assert(~isempty(regexp(type,'^\w*$', 'ONCE')), ...
-    'Expression identifiers only use digits, letters, and underscores.')
-
-count = 1;
-while true
-    id = [type num2str(count)];
-    if ~idExists()
-        uid = id;
-        return
+    % GETUNIQUEID Gets a unique identifier for a block to use in equations
+    %   (rather than having to write the fullpath).
+    %
+    %   Inputs:
+    %       type        The type of identifier we want. (E.g. blocktype)
+    %       existingIDs A bimap object (comes with Logic Simplifier).
+    %                   If existingIDs.lookdown('id') is defined then 'id'
+    %                   is an existing identifier.
+    %
+    %   Output:
+    %       uid     Character array for a unique identifier. Uses a type to
+    %               assign the identifier and appends a number to the end to
+    %               keep it different from existing identifiers.
+    
+    assert(ischar(type), '''type'' variable expected to be char.')
+    assert(~isempty(regexp(type,'^\w*$', 'ONCE')), ...
+        'Expression identifiers only use digits, letters, and underscores.')
+    
+    count = 1;
+    while true
+        id = [type num2str(count)];
+        if ~idExists()
+            uid = id;
+            return
+        end
+        count = count + 1;
     end
-    count = count + 1;
-end
     
     function bool = idExists()
-        try 
+        try
             existingIDs.lookdown(id);
             bool = 1;
         catch ME
@@ -379,10 +377,10 @@ end
             end
         end
     end
-
-% Later, find a uid within a string with:
-%   regexp(str,[type num2str(#) '[^0-9A-Za-z_]'])
-% E.g. For uid From10: 
-%   regexp(str,['From' num2str(10) '[^0-9A-Za-z_]'])
-% '[^0-9A-Za-z_]' is to prohibit matching From100 for From10
+    
+    % Later, find a uid within a string with:
+    %   regexp(str,[type num2str(#) '[^0-9A-Za-z_]'])
+    % E.g. For uid From10:
+    %   regexp(str,['From' num2str(10) '[^0-9A-Za-z_]'])
+    % '[^0-9A-Za-z_]' is to prohibit matching From100 for From10
 end
