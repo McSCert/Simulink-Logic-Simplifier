7064e78c9287fe4199a0fc1012c7c2917422f8ff
Gordon Marks <gor.marks@gmail.com>
2018-05-17 07:59:42 +0000

    Added some comments and identified bugs to fix.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/SimulinkLogicSimplifier@12789 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/secondPass.m
+++ src/secondPass.m
@@ -5,20 +5,23 @@ function secondPass(sys)
     %Looks for consecutive and blocks
     flag = true;
     andBlocks = find_system(sys, 'SearchDepth', 1, 'BlockType', 'Logic', 'Operator', 'AND');
-    for i = 1:length(andBlocks)
+    for i = 1:length(andBlocks) % for all and blocks
+        % Get dst block
         outLine = get_param(andBlocks{i}, 'LineHandles');
         outLine = outLine.Outport;
         dstBlock = get_param(outLine, 'DstBlockHandle');
-        if (~isempty(dstBlock))&&(~(length(dstBlock)>1))
+        
+        % 
+        if (~isempty(dstBlock))&&(~(length(dstBlock)>1)) % if there is 1 dst block
             dstBlockType = get_param(dstBlock, 'BlockType');
             if strcmp(dstBlockType, 'Logic')
                 dstBlockOp = get_param(dstBlock, 'Operator');
-                if strcmp(dstBlockOp, 'AND')
+                if strcmp(dstBlockOp, 'AND') % if the block is a Logic block with AND operator
                     %set new number of inports on destination block
-                    numNewInputs = get_param(andBlocks{i}, 'Inputs');
+                    numNewInputs = str2num(get_param(andBlocks{i}, 'Inputs'));
                     numOldInputs = get_param(dstBlock, 'Inputs');
                     numOldInputs = str2num(numOldInputs)-1;
-                    numInputs = num2str(numOldInputs + str2num(numNewInputs));
+                    numInputs = num2str(numOldInputs + numNewInputs);
                     set_param(dstBlock, 'Inputs', numInputs);
                     
                     %get rid of old line
@@ -30,6 +33,11 @@ function secondPass(sys)
                     srcLines = get_param(andBlocks{i}, 'LineHandles');
                     srcLines = srcLines.Inport;
                     for j = 1:length(dstPorts)-numOldInputs
+                        % 2 bugs:
+                        % if line into andBlocks{i} is branching
+                        % and if connections into dstBlock aren't for
+                        % j+numOldInputs e.g. inport 1 and 4 rather than 3
+                        % and 4
                         source = get_param(srcLines(j), 'SrcPortHandle');
                         add_line(sys, source, dstPorts(j+numOldInputs));
                     end
