34d97d49ae274d4092705d0949582690d7eec9d8
Gordon Marks <gor.marks@gmail.com>
2018-08-27 17:51:43 +0000

    Set params of generated blocks to not require same datatype from inputs.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/SimulinkLogicSimplifier@13808 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/CreatingEquations/addLogicalBlock.m
+++ src/CreatingEquations/addLogicalBlock.m
@@ -4,6 +4,10 @@ function block = addLogicalBlock(opType, sys, varargin)
     %make sure added operator block has a unique name associated with it
     block = add_block('built-in/Logic', getGenBlockName(sys, opType), 'MAKENAMEUNIQUE','ON', varargin{:});
     
+    % temp - eventually pass this with varargin, and potentially avoid the need
+    % for it
+    set_param(block, 'AllPortsSameDT', 'off')
+    
     %set operator
     set_param(block, 'Operator', opType);
 
--- src/CreatingEquations/addRelationalBlock.m
+++ src/CreatingEquations/addRelationalBlock.m
@@ -4,6 +4,10 @@ function block = addRelationalBlock(opType, sys, varargin)
     %make sure added operator block has a unique name associated with it
     block = add_block('built-in/RelationalOperator', getGenBlockName(sys, 'RelationalOp'), 'MAKENAMEUNIQUE','ON', varargin{:});
 
+    % temp - eventually pass this with varargin, and potentially avoid the need
+    % for it
+    set_param(block, 'InputSameDT', 'off')
+    
     %set operator
     set_param(block, 'Operator', opType);
 
